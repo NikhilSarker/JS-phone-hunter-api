fd16ea34273a5ba453ab6bec856453cc152507cf
Nikhil Sarker <100969232+NikhilSarker@users.noreply.github.com>
2023-08-27 10:01:44 -0400

    api link fetch and search box added

--- js/main.js
+++ js/main.js
@@ -1,9 +1,40 @@
 
-const phoneLoad = async () => {
-  const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const phoneLoad = async (searchText) => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
   const data = await res.json();
   const phones = data.data;
-  console.log(phones);
+  displayPhone(phones);
 }
 
-phoneLoad();
+
+const displayPhone = (phones) => {
+  // Get the div container
+  const phone_container = document.getElementById('phone_container');
+  phone_container.textContent = '';
+
+  phones.forEach(phone => {
+    // Create a div
+    const phone_div = document.createElement('div');
+    phone_div.classList = 'card bg-base-100 shadow-xl text-center';
+    phone_div.innerHTML = `
+    <figure><img src="${phone.image}" alt="Shoes" /></figure>
+      <div class="card-body">
+        <h2 class="card-title">${phone.phone_name}</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    `;
+    // AppendChild
+    phone_container.appendChild(phone_div);    
+  });
+}
+  // Search Button
+
+  const handleSearch = () => {
+    const search_field = document.getElementById('search_field');
+    const search_value = search_field.value;
+    phoneLoad(search_value);
+  }
+
